d2413c2924bad1278e8cff0f54aae817a72cd92c
Apostolos Lazidis <apostolos.lazidis@eurodyn.com>
2025-04-06 19:57:47 +0300

    feat: add optiscaler tests getFaultToleranceNodes

---
+++ scheduler/tests/optiScaler/getFaultToleranceNodes.test.ts
@@ -0,0 +1,79 @@
+import { FileSystemHandler } from '../../src/adapters/filesystem';
+import { KubernetesAdapterImpl } from '../../src/adapters/k8s';
+import { PrometheusAdapterImpl } from '../../src/adapters/prometheus';
+import { OptiScaler } from '../../src/core/optiScaler';
+import { ScaleAction } from '../../src/core/optiScaler/enums';
+import { MetricsType } from '../../src/enums';
+import { DummyCluster } from './data/cluster';
+import { DummyDeployments } from './data/deployment';
+
+let k8s: KubernetesAdapterImpl;
+let prometheus: PrometheusAdapterImpl;
+let fileSystem = new FileSystemHandler();
+
+beforeAll(() => {
+  k8s = new KubernetesAdapterImpl();
+  prometheus = new PrometheusAdapterImpl();
+});
+
+const weights = {
+  CPU: 0.5,
+  Memory: 0.5,
+};
+
+describe(`OptiScaler => getFaultToleranceNodes`, () => {
+  test('Fault Tolerance by CPU', () => {
+    const data = {
+      deployment: 'frontend',
+      namespace: 'online-boutique',
+      replicaPods: DummyDeployments['frontend'],
+      nodeMetrics: DummyCluster.Nodes,
+      zonesNodes: DummyCluster.AzTopology,
+      nodesLatency: DummyCluster.NodesLatency,
+    };
+    const optiScaler = new OptiScaler(ScaleAction.UP, data, { prom: prometheus, k8s: k8s, fileSystem });
+
+    const ftNodes = optiScaler.getFaultToleranceNodes(MetricsType.CPU, weights);
+
+    expect(ftNodes.length).toBe(3);
+    expect(ftNodes.some((node) => node === 'node1')).toBe(true);
+    expect(ftNodes.some((node) => node === 'node2')).toBe(true);
+    expect(ftNodes.some((node) => node === 'node3')).toBe(true);
+  });
+  test('Fault Tolerance by MEMORY', () => {
+    const data = {
+      deployment: 'frontend',
+      namespace: 'online-boutique',
+      replicaPods: DummyDeployments['frontend'],
+      nodeMetrics: DummyCluster.Nodes,
+      zonesNodes: DummyCluster.AzTopology,
+      nodesLatency: DummyCluster.NodesLatency,
+    };
+    const optiScaler = new OptiScaler(ScaleAction.UP, data, { prom: prometheus, k8s: k8s, fileSystem });
+
+    const ftNodes = optiScaler.getFaultToleranceNodes(MetricsType.MEMORY, weights);
+
+    expect(ftNodes.length).toBe(3);
+    expect(ftNodes.some((node) => node === 'node1')).toBe(true);
+    expect(ftNodes.some((node) => node === 'node2')).toBe(true);
+    expect(ftNodes.some((node) => node === 'node3')).toBe(true);
+  });
+  test('Fault Tolerance by CPU-MEMORY', () => {
+    const data = {
+      deployment: 'frontend',
+      namespace: 'online-boutique',
+      replicaPods: DummyDeployments['frontend'],
+      nodeMetrics: DummyCluster.Nodes,
+      zonesNodes: DummyCluster.AzTopology,
+      nodesLatency: DummyCluster.NodesLatency,
+    };
+    const optiScaler = new OptiScaler(ScaleAction.UP, data, { prom: prometheus, k8s: k8s, fileSystem });
+
+    const ftNodes = optiScaler.getFaultToleranceNodes(MetricsType.CPU_MEMORY, weights);
+
+    expect(ftNodes.length).toBe(3);
+    expect(ftNodes.some((node) => node === 'node1')).toBe(true);
+    expect(ftNodes.some((node) => node === 'node2')).toBe(true);
+    expect(ftNodes.some((node) => node === 'node3')).toBe(true);
+  });
+});
